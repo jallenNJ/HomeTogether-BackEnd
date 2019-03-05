bef7588e35a41533fbf24bdcf1869be748c35650
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2019-03-05 14:14:01 -0500

    Implement the ability to find users from a partial string

--- HomeTogetherServer/routes/users.js
+++ HomeTogetherServer/routes/users.js
@@ -4,8 +4,11 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', async (req, res, next)=> {
   //TODO: Add log in check
+
+  var query = (req.query.username)?
+    {user:{$regex:req.query.username}}:{};
   try{
-    var users = await req.collections.users.find({}).project({_id:0, pass:0}).toArray();
+    var users = await req.collections.users.find(query).project({_id:0, pass:0}).toArray();
 
   } catch(ex){
     console.error(ex);
