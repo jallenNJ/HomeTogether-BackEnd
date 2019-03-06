b87ff0aaeb0e715cf3da45ce3a7cb0c94e05f100
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2019-03-06 12:05:43 -0500

    Fixed deprecation warnings on server start

--- HomeTogetherServer/app.js
+++ HomeTogetherServer/app.js
@@ -30,7 +30,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser("homeTogether"));
-app.use(ex_session({ secret: 'homeTogether' }));
+app.use(ex_session({ secret: 'homeTogether', resave: false, saveUninitialized:false })); //If touch/rolling is enabled, set resave to true
 app.use(express.static(path.join(__dirname, 'public')));
 
 
