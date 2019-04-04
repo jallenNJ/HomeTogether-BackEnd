5f48068a3d4425520fbb7f848ceeb2fd653b7718
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2019-04-04 12:24:58 -0400

    Added form to the field and limited selection to one

--- HomeTogetherServer/public/javascripts/HomeTogetherClient.js
+++ HomeTogetherServer/public/javascripts/HomeTogetherClient.js
@@ -5,6 +5,7 @@ $(document).ready(()=>{
     body = $("body");
     const click = ()=>{
         $(document).off("keypress");
+
         $.post(
             "/login",
             $("form").serialize(),
@@ -22,7 +23,6 @@ $(document).ready(()=>{
                     }
                 });
             });
-
         }        
     $("button").on("click", click);
     $(document).on("keypress", (key)=>{
@@ -32,10 +32,11 @@ $(document).ready(()=>{
     });    
 });
 
-function  selectHousehold(){
+function selectHousehold(){
     $.get("/household",
     {id:$(this).data("id"), activeData:true},
-    (data)=>{houseData = data.house; 
+    (data)=>{
+        houseData = data.house; 
         delete houseData.pantry;
         loadHouse();}
     );
@@ -44,39 +45,57 @@ function  selectHousehold(){
 function loadHouse(){
     body.empty();
     body.append($("<p> Do member bar</p>"));
-    body.append($("<button> Pantry </button>")).on("click", $.get("/household/pantry", {}, (data)=>{loadPantry(data.pantry)}));
+    body.append($("<button> Pantry </button>")).on("click", $.get("/household/pantry", {}, (data)=>{ 
+        $("button").off("click");
+        loadPantry(data.pantry)}
+        ));
 }
 
 function loadPantry(pantryData){
     body.empty();
     let keys = ["name", "quantity", "expires", "category", "tags"];
     generateTable(pantryData, keys);
-   
+    generatePantryForm(keys);
+
+    body.append($("<button></button>").text("Delete"));
+    body.append($("<button></button>").text("Update"));
+    body.append($("<button></button>").text("Create"));
 }
 
 function generateTable(tableData, keys){
     let table = ($("<table></table>"));
     body.append(table);
    
-
     const generateRow = (rowData) =>{
         let row = $("<tr></tr>");
         for(let key of keys){
             let cell = rowData? $("<td></td>").text(rowData[key]):$("<th></th>").text(key); 
             row.append(cell);
         }
-
         return row;
-
     }
     let tHead = $("<thead></thead>");
     tHead.append(generateRow(undefined));
     table.append(tHead);
     let tBody = $("<tbody></tbody>");
-    tBody.on("click", "tr", function(){$(this).toggleClass("selectedItem"); return false;});
+    tBody.on("click", "tr", function(){
+        $(".selectedItem").removeClass();
+        $(this).toggleClass("selectedItem"); 
+        return false;
+    });
     table.append(tBody);
     for(let rowData of tableData){
         tBody.append(generateRow(rowData));
     }
-   
+}
+
+function generatePantryForm(keys){
+    let form = $("<form></form>");
+    for(let key of keys){
+        let id = "pf"+key;
+        form.append($("<label></label>").prop("for", id).text(key));
+        form.append($("<input></input>").prop("id", id));
+
+    }
+    body.append(form);
 }
